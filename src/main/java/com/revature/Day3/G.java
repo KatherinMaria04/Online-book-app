dad5a7d7c20a3364dbd3e73df4491733b82da365
katherin <katherinmaria04@gmail.com>
2019-08-14 01:52:01 -0400

    added modifed src

--- src/main/java/com/revature/Day3/G.java
+++ src/main/java/com/revature/Day3/G.java
@@ -1,5 +1,16 @@
 package com.revature.Day3;
 
 public class G {
+	public static void main(String[] args) {
+		String d ="naresh";
+		if ( d == null || d.equals(""))
+		{
+			System.out.println("invalid");
+		}
+		else {
+			System.out.println("valid");
+		}
+		
+	}
 
 }
